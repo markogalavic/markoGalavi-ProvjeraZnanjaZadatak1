414750b065f53ef6050be7e3eec83e0e7ec07f8e
markogalavic <55134792+markogalavic@users.noreply.github.com>
2020-02-13 09:58:14 +0100

    Add files via upload

---
+++ markoGalavićProvjeraZnanjaZadatak1/markoGalavićProvjeraZnanjaZadatak1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace markoGalavićProvjeraZnanjaZadatak1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=10;
+            int b=7;
+            double c=12.5;
+            double d=16.7;
+            double n;
+            double f;
+            n = a + b + c + d;
+            f = n / 4;
+            Console.WriteLine("Prosjek je:" + f);
+            Console.ReadKey();
+            
+        }
+    }
+}
